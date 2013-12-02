9c9e3383edf0944ae4f9e160ba962de947120532
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2013-12-02 20:57:31 +0000

    fix the last brooklyn-camp compile error by moving class with brooklyn-launcher dependendency to brooklyn-launcher

---
+++ usage/launcher/src/test/java/brooklyn/launcher/YamlLauncher.java
@@ -0,0 +1,27 @@
+package brooklyn.launcher;
+
+import com.google.common.annotations.Beta;
+
+import io.brooklyn.camp.brooklyn.spi.creation.BrooklynAssemblyTemplateInstantiator;
+
+/** convenience for launching YAML files directly */
+@Beta
+public class YamlLauncher extends YamlLauncherAbstract {
+
+    @Override
+    protected BrooklynCampPlatformLauncherAbstract newPlatformLauncher() {
+        return new BrooklynCampPlatformLauncher();
+    }
+
+    public static void main(String[] args) {
+        BrooklynAssemblyTemplateInstantiator.TARGET_LOCATION = "localhost";
+        
+        YamlLauncher l = new YamlLauncher();
+        
+//        l.launchAppYaml("java-web-app-and-db-with-function.yaml");
+//        l.launchAppYaml("java-web-app-and-memsql.yaml");
+//        l.launchAppYaml("memsql.yaml");
+        l.launchAppYaml("playing.yaml");
+    }
+    
+}
