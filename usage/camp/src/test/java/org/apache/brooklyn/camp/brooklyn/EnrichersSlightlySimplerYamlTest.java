4f6f643b614cba4b4ae028b13bcd3c0238040c69
John McCabe <john@johnmccabe.net>
2015-12-13 19:38:14 +0000

    disable CAMP REST API tests that are pulling in brooklyn-library dependencies

--- usage/camp/src/test/java/org/apache/brooklyn/camp/brooklyn/EnrichersSlightlySimplerYamlTest.java
+++ usage/camp/src/test/java/org/apache/brooklyn/camp/brooklyn/EnrichersSlightlySimplerYamlTest.java
@@ -16,118 +16,118 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.brooklyn.camp.brooklyn;
+// package org.apache.brooklyn.camp.brooklyn;
 
-import java.net.URI;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+// import java.net.URI;
+// import java.util.Collection;
+// import java.util.Iterator;
+// import java.util.List;
 
-import org.apache.brooklyn.api.entity.Entity;
-import org.apache.brooklyn.core.entity.Attributes;
-import org.apache.brooklyn.core.entity.Entities;
-import org.apache.brooklyn.core.entity.EntityInternal;
-import org.apache.brooklyn.core.sensor.Sensors;
-import org.apache.brooklyn.entity.group.DynamicCluster;
-import org.apache.brooklyn.entity.webapp.JavaWebAppSoftwareProcess;
-import org.apache.brooklyn.test.EntityTestUtils;
-import org.apache.brooklyn.util.collections.CollectionFunctionals;
-import org.apache.brooklyn.util.collections.MutableList;
-import org.apache.brooklyn.util.math.MathPredicates;
-import org.apache.brooklyn.util.text.StringPredicates;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.testng.Assert;
-import org.testng.annotations.Test;
+// import org.apache.brooklyn.api.entity.Entity;
+// import org.apache.brooklyn.core.entity.Attributes;
+// import org.apache.brooklyn.core.entity.Entities;
+// import org.apache.brooklyn.core.entity.EntityInternal;
+// import org.apache.brooklyn.core.sensor.Sensors;
+// import org.apache.brooklyn.entity.group.DynamicCluster;
+// import org.apache.brooklyn.entity.webapp.JavaWebAppSoftwareProcess;
+// import org.apache.brooklyn.test.EntityTestUtils;
+// import org.apache.brooklyn.util.collections.CollectionFunctionals;
+// import org.apache.brooklyn.util.collections.MutableList;
+// import org.apache.brooklyn.util.math.MathPredicates;
+// import org.apache.brooklyn.util.text.StringPredicates;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.testng.Assert;
+// import org.testng.annotations.Test;
 
-import com.google.common.base.Predicate;
-import com.google.common.base.Predicates;
-import com.google.common.collect.Iterables;
+// import com.google.common.base.Predicate;
+// import com.google.common.base.Predicates;
+// import com.google.common.collect.Iterables;
 
-/** Tests some improvements to enricher classes to make them a bit more yaml friendly.
- * Called "SlightlySimpler" as it would be nice to make enrichers a lot more yaml friendly! */
-@Test
-public class EnrichersSlightlySimplerYamlTest extends AbstractYamlTest {
-    private static final Logger log = LoggerFactory.getLogger(EnrichersSlightlySimplerYamlTest.class);
+// /** Tests some improvements to enricher classes to make them a bit more yaml friendly.
+//  * Called "SlightlySimpler" as it would be nice to make enrichers a lot more yaml friendly! */
+// @Test
+// public class EnrichersSlightlySimplerYamlTest extends AbstractYamlTest {
+//     private static final Logger log = LoggerFactory.getLogger(EnrichersSlightlySimplerYamlTest.class);
 
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-    @Test
-    public void testWithAppEnricher() throws Exception {
-        Entity app = createAndStartApplication(loadYaml("test-app-with-enrichers-slightly-simpler.yaml"));
-        waitForApplicationTasks(app);
-        log.info("Started "+app+":");
-        Entities.dumpInfo(app);
+//     @SuppressWarnings({ "unchecked", "rawtypes" })
+//     @Test
+//     public void testWithAppEnricher() throws Exception {
+//         Entity app = createAndStartApplication(loadYaml("test-app-with-enrichers-slightly-simpler.yaml"));
+//         waitForApplicationTasks(app);
+//         log.info("Started "+app+":");
+//         Entities.dumpInfo(app);
         
-        Entity cluster = Iterables.getOnlyElement( app.getChildren() );
-        Collection<Entity> leafs = ((DynamicCluster)cluster).getMembers();
-        Iterator<Entity> li = leafs.iterator();
+//         Entity cluster = Iterables.getOnlyElement( app.getChildren() );
+//         Collection<Entity> leafs = ((DynamicCluster)cluster).getMembers();
+//         Iterator<Entity> li = leafs.iterator();
         
-        Entity e1 = li.next();
-        ((EntityInternal)e1).sensors().set(Sensors.newStringSensor("ip"), "127.0.0.1");
-        EntityTestUtils.assertAttributeEqualsEventually(e1, Sensors.newStringSensor("url"), "http://127.0.0.1/");
-        EntityTestUtils.assertAttributeEqualsEventually(e1, Attributes.MAIN_URI, URI.create("http://127.0.0.1/"));
+//         Entity e1 = li.next();
+//         ((EntityInternal)e1).sensors().set(Sensors.newStringSensor("ip"), "127.0.0.1");
+//         EntityTestUtils.assertAttributeEqualsEventually(e1, Sensors.newStringSensor("url"), "http://127.0.0.1/");
+//         EntityTestUtils.assertAttributeEqualsEventually(e1, Attributes.MAIN_URI, URI.create("http://127.0.0.1/"));
 
-        int i=2;
-        while (li.hasNext()) {
-            Entity ei = li.next();
-            ((EntityInternal)ei).sensors().set(Sensors.newStringSensor("ip"), "127.0.0."+i);
-            i++;
-        }
+//         int i=2;
+//         while (li.hasNext()) {
+//             Entity ei = li.next();
+//             ((EntityInternal)ei).sensors().set(Sensors.newStringSensor("ip"), "127.0.0."+i);
+//             i++;
+//         }
         
-        EntityTestUtils.assertAttributeEventually(cluster, Sensors.newSensor(Iterable.class, "urls.list"),
-            (Predicate)CollectionFunctionals.sizeEquals(3));
+//         EntityTestUtils.assertAttributeEventually(cluster, Sensors.newSensor(Iterable.class, "urls.list"),
+//             (Predicate)CollectionFunctionals.sizeEquals(3));
         
-        EntityTestUtils.assertAttributeEventually(cluster, Sensors.newSensor(String.class, "urls.list.comma_separated.max_2"),
-            StringPredicates.matchesRegex("\"http:\\/\\/127[^\"]*\\/\",\"http:\\/\\/127[^\"]*\\/\""));
+//         EntityTestUtils.assertAttributeEventually(cluster, Sensors.newSensor(String.class, "urls.list.comma_separated.max_2"),
+//             StringPredicates.matchesRegex("\"http:\\/\\/127[^\"]*\\/\",\"http:\\/\\/127[^\"]*\\/\""));
 
-        EntityTestUtils.assertAttributeEventually(cluster, Attributes.MAIN_URI, Predicates.notNull());
-        URI main = cluster.getAttribute(Attributes.MAIN_URI);
-        Assert.assertTrue(main.toString().matches("http:\\/\\/127.0.0..\\/"), "Wrong URI: "+main);
+//         EntityTestUtils.assertAttributeEventually(cluster, Attributes.MAIN_URI, Predicates.notNull());
+//         URI main = cluster.getAttribute(Attributes.MAIN_URI);
+//         Assert.assertTrue(main.toString().matches("http:\\/\\/127.0.0..\\/"), "Wrong URI: "+main);
         
-        EntityTestUtils.assertAttributeEventually(app, Attributes.MAIN_URI, Predicates.notNull());
-        main = app.getAttribute(Attributes.MAIN_URI);
-        Assert.assertTrue(main.toString().matches("http:\\/\\/127.0.0..\\/"), "Wrong URI: "+main);
+//         EntityTestUtils.assertAttributeEventually(app, Attributes.MAIN_URI, Predicates.notNull());
+//         main = app.getAttribute(Attributes.MAIN_URI);
+//         Assert.assertTrue(main.toString().matches("http:\\/\\/127.0.0..\\/"), "Wrong URI: "+main);
         
-        // TODO would we want to allow "all-but-usual" as the default if nothing specified
-    }
+//         // TODO would we want to allow "all-but-usual" as the default if nothing specified
+//     }
     
-    @Test(groups="Integration")
-    public void testWebappWithAveragingEnricher() throws Exception {
-        Entity app = createAndStartApplication(loadYaml("test-webapp-with-averaging-enricher.yaml"));
-        waitForApplicationTasks(app);
-        log.info("Started "+app+":");
-        Entities.dumpInfo(app);
+//     @Test(groups="Integration")
+//     public void testWebappWithAveragingEnricher() throws Exception {
+//         Entity app = createAndStartApplication(loadYaml("test-webapp-with-averaging-enricher.yaml"));
+//         waitForApplicationTasks(app);
+//         log.info("Started "+app+":");
+//         Entities.dumpInfo(app);
 
-        List<JavaWebAppSoftwareProcess> appservers = MutableList.copyOf(Entities.descendants(app, JavaWebAppSoftwareProcess.class));
-        Assert.assertEquals(appservers.size(), 3);
+//         List<JavaWebAppSoftwareProcess> appservers = MutableList.copyOf(Entities.descendants(app, JavaWebAppSoftwareProcess.class));
+//         Assert.assertEquals(appservers.size(), 3);
         
-        EntityInternal srv0 = (EntityInternal) appservers.get(0);
-        EntityInternal dwac = (EntityInternal) srv0.getParent();
-        EntityInternal cdwac = (EntityInternal) dwac.getParent();
+//         EntityInternal srv0 = (EntityInternal) appservers.get(0);
+//         EntityInternal dwac = (EntityInternal) srv0.getParent();
+//         EntityInternal cdwac = (EntityInternal) dwac.getParent();
         
-        srv0.sensors().set(Sensors.newDoubleSensor("my.load"), 20.0);
+//         srv0.sensors().set(Sensors.newDoubleSensor("my.load"), 20.0);
         
-        EntityTestUtils.assertAttributeEventually(dwac, Sensors.newSensor(Double.class, "my.load.averaged"),
-            MathPredicates.equalsApproximately(20));
-        EntityTestUtils.assertAttributeEventually(cdwac, Sensors.newSensor(Double.class, "my.load.averaged"),
-            MathPredicates.equalsApproximately(20));
+//         EntityTestUtils.assertAttributeEventually(dwac, Sensors.newSensor(Double.class, "my.load.averaged"),
+//             MathPredicates.equalsApproximately(20));
+//         EntityTestUtils.assertAttributeEventually(cdwac, Sensors.newSensor(Double.class, "my.load.averaged"),
+//             MathPredicates.equalsApproximately(20));
 
-        srv0.sensors().set(Sensors.newDoubleSensor("my.load"), null);
-        EntityTestUtils.assertAttributeEventually(cdwac, Sensors.newSensor(Double.class, "my.load.averaged"),
-            Predicates.isNull());
+//         srv0.sensors().set(Sensors.newDoubleSensor("my.load"), null);
+//         EntityTestUtils.assertAttributeEventually(cdwac, Sensors.newSensor(Double.class, "my.load.averaged"),
+//             Predicates.isNull());
 
-        ((EntityInternal) appservers.get(1)).sensors().set(Sensors.newDoubleSensor("my.load"), 10.0);
-        ((EntityInternal) appservers.get(2)).sensors().set(Sensors.newDoubleSensor("my.load"), 20.0);
-        EntityTestUtils.assertAttributeEventually(cdwac, Sensors.newSensor(Double.class, "my.load.averaged"),
-            MathPredicates.equalsApproximately(15));
-        srv0.sensors().set(Sensors.newDoubleSensor("my.load"), 0.0);
-        EntityTestUtils.assertAttributeEventually(cdwac, Sensors.newSensor(Double.class, "my.load.averaged"),
-            MathPredicates.equalsApproximately(10));
-    }
+//         ((EntityInternal) appservers.get(1)).sensors().set(Sensors.newDoubleSensor("my.load"), 10.0);
+//         ((EntityInternal) appservers.get(2)).sensors().set(Sensors.newDoubleSensor("my.load"), 20.0);
+//         EntityTestUtils.assertAttributeEventually(cdwac, Sensors.newSensor(Double.class, "my.load.averaged"),
+//             MathPredicates.equalsApproximately(15));
+//         srv0.sensors().set(Sensors.newDoubleSensor("my.load"), 0.0);
+//         EntityTestUtils.assertAttributeEventually(cdwac, Sensors.newSensor(Double.class, "my.load.averaged"),
+//             MathPredicates.equalsApproximately(10));
+//     }
     
-    @Override
-    protected Logger getLogger() {
-        return log;
-    }
+//     @Override
+//     protected Logger getLogger() {
+//         return log;
+//     }
     
-}
+// }
